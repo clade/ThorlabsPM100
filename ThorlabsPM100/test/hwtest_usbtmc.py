38e58ef2b8c040048ecac4a225f4deaed67aeea0
Pierre Cladé <pierre.clade@spectro.jussieu.fr>
2021-06-16 17:17:17 +0200

    Add test of configuration

--- ThorlabsPM100/test/hwtest_usbtmc.py
+++ ThorlabsPM100/test/hwtest_usbtmc.py
@@ -19,3 +19,16 @@ class TestHW(unittest.TestCase):
             power_meter.sense.correction.wavelength = 10
 
         self.assertIn('Wavelength is', str(context.exception))
+
+    def test_configuration(self):
+        power_meter.configure.scalar.power()
+        self.assertEqual(power_meter.getconfigure, 'POW')
+        power_meter.configure.scalar.current.dc()
+        self.assertEqual(power_meter.getconfigure, 'CURR')
+
+if __name__=='__main__':
+    power_meter.configure.scalar.power()
+    print(power_meter._inst.query('READ?'))
+    power_meter.configure.scalar.current.dc()
+    print(power_meter.getconfigure)
+
